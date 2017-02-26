a8b151f01629ed94ee7071932df9721beedfc82b
TomoGaSukunai <292768995@qq.com>
2017-02-27 00:39:21 +0800

    User simpler  source

--- Tools/Audio/audio5.js
+++ Tools/Audio/audio5.js
@@ -2,11 +2,16 @@ var auCtx = new AudioContext()
 var analyser = auCtx.createAnalyser()
 var proccesr = auCtx.createScriptProcessor(1024, 1, 1)
 var source1 = auCtx.createOscillator()
-source1.frequency = 1760
+source1.frequency.value = 440 * 1.5
+//source1.type = "square"
 var source2 = auCtx.createOscillator()
-source2.frequency = 440
+source2.frequency.value = 440 * 1.75
+//source2.type = "square"
 var gain = auCtx.createGain()
-gain.gain.value = 0.4
+gain.gain.value = 0.2
+
+var gain2 = auCtx.createGain()
+gain2.gain.value = 0.01
 var canvas = document.getElementById("gaming")
 var ctx = canvas.getContext("2d")
 
@@ -22,16 +27,19 @@ source1.connect(gain)
 source2.connect(gain)
 gain.connect(analyser)
 analyser.connect(proccesr)
-proccesr.connect(auCtx.destination)
+proccesr.connect(gain2)
+gain2.connect(auCtx.destination)
 analyser.fftSize = 2048
 var floatTime = new Float32Array(analyser.frequencyBinCount)
 var floatFreq = new Float32Array(analyser.frequencyBinCount)
 
+source1.start(1)
+source2.start(1)
 
 proccesr.onaudioprocess = function(e){
     analyser.getFloatFrequencyData(floatFreq)
     analyser.getFloatTimeDomainData(floatTime)
-    auCtx.suspend().then(function(){ })
+    //auCtx.suspend().then(function(){ })
     var inputBuffer = e.inputBuffer
     var outputBuffer = e.outputBuffer
     var inputData = inputBuffer.getChannelData(0)
@@ -41,21 +49,29 @@ proccesr.onaudioprocess = function(e){
     }
     drawData(floatFreq.slice(0).map(x=>(x+200)/200), ctx, Areas[0])
     drawData(floatTime, ctx, Areas[2])
-    drawData(inputData, ctx, Areas[3])
+    //drawData(inputData, ctx, Areas[3])
 }
 
+
+
+let status = false
 canvas.onclick = function(){
-    auCtx.resume()
+    if (status) 
+        auCtx.suspend().then(()=>{status = false})
+    else
+        auCtx.resume().then(()=>{status = true})
 }
 
-source1.start()
-source2.start()
+
 
 var fs = require("fs")
 var data = new Uint8Array(Buffer.from(fs.readFileSync("./tools/audio/data1.base64","UTF-8"),"base64"))
 
-// auCtx.decodeAudioData(data.buffer,function(buffer){
-//     //source.buffer = buffer
-//     source.start()
-// })
+ auCtx.decodeAudioData(data.buffer,function(buffer){
+     var source = auCtx.createBufferSource()
+     source.buffer = buffer
+     source.loop = true
+     source.connect(gain)
+     source.start()
+ })
 
