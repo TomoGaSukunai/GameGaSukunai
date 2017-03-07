e43daa16f753c3cd74d10115659d137026cf0c53
TomoGaSukunai <292768995@qq.com>
2017-03-07 12:39:28 +0800

    Speed up by refactor and memorify

--- Tools/PlayNote/PlayNote.js
+++ Tools/PlayNote/PlayNote.js
@@ -1,4 +1,5 @@
 var drawing = require("../../lib/drawing")
+var note = require("./note")
 
 var then = Date.now()
 var audioContext = new AudioContext()
@@ -8,9 +9,8 @@ var ctx = canvas.getContext("2d")
 var canvasArea = new drawing.CanvasArea(canvas)
 
 function playData(data, node){
-
     var buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate)
-    var signal = buffer.getChannelData(0)
+    var signal = buffer.getChannelData(0)    
     for (var i=0;i<data.length;i++){
         signal[i] = data[i]
     }
@@ -44,8 +44,8 @@ function createNote(freq, envelope){
     return res
 }
 
-function createEnvelope(n_eighth){
-    var sustain = audioContext.sampleRate*2/16*n_eighth - 1000-500-1000
+function createEnvelope(n_sixteenth){
+    var sustain = audioContext.sampleRate*2/16*n_sixteenth - 1000-500-1000
     return {
         attack: 1000,
         decay: 500,
@@ -104,17 +104,14 @@ function matParse(mat){
     //console.log(name && getFreqD(name,plus),env)    
     return createNote(name && getFreqD(name,plus),envNames[env])
 }
-var matnotes = require("./matnotes").split(" ").map(x=>matParse(x))
-var cello = "do1f do1f so0f so0f la0f la0f mi0f mi0f fa0f fa0f do0f do0f fa0f fa0f so0f so0f".split(" ").map(x=>matParse(x))
-var blkblock = "blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf".split(" ").map(x=>matParse(x))
-var violin1 = [...blkblock, ...blkblock, ...blkblock, ...matnotes]
-var violin2 = [...blkblock, ...blkblock, ...matnotes, ...blkblock]
-var violin3 = [...blkblock, ...matnotes, ...blkblock, ...blkblock]
-var cellos = []
-for (var i=0; i<23; i++){
-    cellos = cellos.concat(cello)
-}
-cellos = cellos.concat(matParse("blkf"))
+// var matnotes = require("./matnotes").split(" ").map(x=>matParse(x))
+// var cello = "do1f do1f so0f so0f la0f la0f mi0f mi0f fa0f fa0f do0f do0f fa0f fa0f so0f so0f".split(" ").map(x=>matParse(x))
+// var blkblock = "blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf".split(" ").map(x=>matParse(x))
+// var violin1 = [...blkblock, ...blkblock, ...blkblock, ...matnotes]
+// var violin2 = [...blkblock, ...blkblock, ...matnotes, ...blkblock]
+// var violin3 = [...blkblock, ...matnotes, ...blkblock, ...blkblock]
+// var cellos = [repeat(cello, 23), matParse("blkf")]
+
 function playSigss(node, ...sigss){
     var c = sigss.length
     var n = sigss[0].reduce((a,b)=>a+b.length,0)
@@ -137,12 +134,81 @@ function drawFreq(){
     requestAnimationFrame(drawFreq)
 
     anylser.getByteFrequencyData(anaF)
-    var holeArea = canvasArea.getHoleArea()
-    drawing.clearArea(ctx, holeArea)
-    drawing.drawByteBars(anaF, ctx, holeArea)
+    var area = canvasArea.getRow(2,0)
+    drawing.clearArea(ctx, area)
+    drawing.drawByteBars(anaF, ctx, area)
     //console.log(anaF.reduce((a,b)=>Math.max(a,b)))
     //console.log(anaF.reduce((a,b)=>Math.min(a,b)))
 }
 drawFreq()
-playSigss(anylser, cellos, violin1, violin2, violin3)
+//playSigss(anylser, cellos, violin1, violin2, violin3)
+//console.log(Date.now() - then)
+
+var t16 = 2/16
+var atk = 0.01
+var dec = 0.005
+var rls = 0.01
+var envelopes = {
+    f: new note.EnvelopeADSR(atk, dec, 4*t16-atk-dec-rls, rls),
+    e: new note.EnvelopeADSR(atk, dec, 2*t16-atk-dec-rls, rls),
+    s: new note.EnvelopeADSR(atk, dec, t16-atk-dec-rls, rls),
+}
+
+//var noteMemo = {}
+function parseNote(mat){
+    var name = noteNames[mat.slice(0,2)]
+    var plus = parseInt(mat[2]) - 1
+    var env = mat[3]
+    var freq = name && getFreqD(name,plus)
+    //console.log(name && getFreqD(name,plus),env)    
+    // if (noteMemo[env] === undefined){
+    //     noteMemo[env] = {}
+    // }
+    // var ref = noteMemo[env]
+    // if (ref[freq] === undefined){
+    //     ref[freq] = new note.Note(freq, envelopes[env])
+    // }
+    // return ref[freq]
+    return new note.Note(freq, envelopes[env])
+}
+function sigsFromNotess(...notess){    
+    var sampleRate = audioContext.sampleRate
+    var totalTime = notess[0].reduce((a,b)=>a+b.envelope.getTotal(),0)
+    var totalN = Math.floor(sampleRate * totalTime)
+    var sigs = new Float32Array(totalN)
+    var c = notess.length
+    for(var notes of notess){
+        var past = 0
+        var idx = 0
+        for (var note of notes){
+            var offset = idx/sampleRate - past
+            // var sig = note.getSigs(offset, sampleRate)
+            // var n = sig.length
+            // for(var i=0; i<n; i++){
+            //     sigs[idx++] += sig[i] /c/2
+            // }
+            idx = note.plusSigs(sigs, idx, offset, sampleRate, c)
+            past += note.envelope.getTotal()
+        }
+    }
+    return sigs
+}
+function repeat(x, n){
+    var r = []
+    for(var i=0; i<n; i++)
+        r = r.concat(x)
+    return r
+}
+
+parseNote = (mat)=>note.NoteLib.parseNote.apply(note.NoteLib,[mat])
+var canon = require("./matnotes").split(" ").map(x=>parseNote(x))
+var cello = "do1f do1f so0f so0f la0f la0f mi0f mi0f fa0f fa0f do0f do0f fa0f fa0f so0f so0f".split(" ").map(x=>parseNote(x))
+var blkblock = "blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf blkf".split(" ").map(x=>parseNote(x))
+
+var violin1 = [...blkblock, ...blkblock, ...blkblock, ...canon]
+var violin2 = [...blkblock, ...blkblock, ...canon, ...blkblock]
+var violin3 = [...blkblock, ...canon, ...blkblock, ...blkblock]
+var cellos = [...repeat(cello,23), parseNote("blkf")]
+console.log(Date.now() - then)
+playData(sigsFromNotess(cellos, violin1, violin2, violin3), anylser)
 console.log(Date.now() - then)
--- Tools/PlayNote/note.js
+++ Tools/PlayNote/note.js
@@ -1,45 +1,55 @@
+let Counter={}
+function count(id){
+    if(Counter[id] === undefined)
+        Counter[id] = 1
+    else
+        Counter[id]++
+}
+
 const Note = class {
     constructor(freq, envelope){
-        this.freq = freq
-        this.w = Math.PI * 2 / freq
+        this.w = Math.PI * 2 *   freq
         this.envelope = envelope
+        this.memo = {}
     }
     getVal(t){
         return Math.sin(t*this.w) * this.envelope.getGain(t)
     }
-    getVals(start, dt, end){
-        let n = Math.floor((end-start)/dt)
-        let res = new Float32Array(n)
-        let ts = new Float32Array(n)        
-        for (let i=0; i<n; i++){
-            ts[i] = dt*i
+    getSigs(offset, sampleRate){
+        count("getSigs")
+        if (this.memo[sampleRate] === undefined){
+            this.memo[sampleRate] = {}
         }
-        let gains = this.envelope.getGains(ts)        
-        for (let i=0; i<n; i++){
-            res[i] = Math.sin(ts[i]*this.w) * gains[i]
+        let temp_memo = this.memo[sampleRate]
+        if (temp_memo[offset] === undefined){
+            count("getSigs.run")            
+            let dt = 1/sampleRate
+            let gains = this.envelope.getGains(offset, sampleRate)
+            let n = gains.length            
+            let res = new Float32Array(n)
+            let t = 0            
+            for (let i=0; i<n; i++){                
+                res[i] = Math.sin(t*this.w) * gains[i]
+                t += dt
+            }
+            temp_memo[offset] = res
+        }        
+        return temp_memo[offset]
+    }
+    plusSigs(array, offsetN, offset, sampleRate, c){
+        count("plusSigs")
+        let sigs = this.getSigs(offset, sampleRate)
+        let n = sigs.length
+        let j = offsetN
+        for (let i=0; i<n; i++, j++){
+            array[j] += sigs[i] /c/2
         }
-        return res
+        return j
     }
 }
 
-const Envelope = class {
-    getGain(t){
-        return 1;
-    }
-    getGains(ts){
-        let res = new Float32Array(ts.length)
-        let idx = 0
-        for (let t of ts){
-            res[idx++] = 1
-        }
-        return res
-    }
-}
-
-
-const EnvelopeADSR = class extends Envelope {
-    constructor(attack, decay, sustain, release){
-        super()
+const EnvelopeADSR = class{
+    constructor(attack, decay, sustain, release){  
         this.ts = [attack, decay, sustain, release, Infinity]
         this.gs = [
             (t) => t/attack,
@@ -48,31 +58,89 @@ const EnvelopeADSR = class extends Envelope {
             (t) => 3/4 *(1-t/release),
             (t) => 0,
         ]
+        this.memo = {}
     }
-    getGain(t){
-        var t = t
-        for (let i in this.ts){            
-            if (t <= this.ts[i]){
-                return this.gs[i](t)
-            }
-            t -= this.ts[i]
+    getGains(offset, sampleRate){
+        count("getGains")
+        if (this.memo[sampleRate] === undefined){
+            this.memo[sampleRate] = {}
         }
-        return 0
-    }
-    getGains(ts){
-        let res = new Float32Array(ts.length)
-        let k = 0        
-        let reduce = 0
-        for(var i=0; i<ts.length; i++){
-            var t = ts[i] - reduce
-            for(;;k++){
-                if (t <= this.ts[k]){
-                    res[i] = this.gs[k](t)
+        let temp_memo = this.memo[sampleRate]
+        if (temp_memo[offset] === undefined){
+            count("getGains.run")
+            let n = Math.floor((this.getTotal()-offset)*sampleRate)
+            let dt = 1/sampleRate
+            let res = new Float32Array(n)
+            let t = 0
+            let k = 0
+            for (let i=0; i<n; i++){                
+                let gain = 0
+                for(;;k++){
+                    if (t < this.ts[k]){
+                        res[i] = this.gs[k](t)
+                        break
+                    }
+                    t -= this.ts[k]                    
                 }
-                t -= this.ts[k]
-                reduce += this.ts[k]
+                t += dt
             }
-        }
-        return res
+            temp_memo[offset] = res
+        }        
+        return temp_memo[offset]
     }
+    getTotal(){
+        return this.ts.reduce((a,b)=>a+(isFinite(b)?b:0))
+    }
+}
+
+
+const noteNames = {
+    do: 1,
+    re: 2,
+    mi: 3,
+    fa: 4,
+    so: 5,
+    la: 6,
+    ti: 7,
+    tb: 6.5,
+    bl: 0,
+}
+const t16 = 2/16
+const atk = 0.01
+const dec = 0.005
+const rls = 0.01
+const envelopes = {
+    f: new EnvelopeADSR(atk, dec, 4*t16-atk-dec-rls, rls),
+    e: new EnvelopeADSR(atk, dec, 2*t16-atk-dec-rls, rls),
+    s: new EnvelopeADSR(atk, dec, t16-atk-dec-rls, rls),
+}
+
+const NoteLib = {
+    memo:{},
+    parseNote(mat){
+        const name = noteNames[mat.slice(0,2)]
+        const plus = parseInt(mat[2]) - 1
+        const env = mat[3]
+        const freq = name && this.getFreqD(name,plus)
+        //console.log(name && getFreqD(name,plus),env)    
+        if (this.memo[env] === undefined){
+            this.memo[env] = {}
+        }
+        const ref = this.memo[env]
+        if (ref[freq] === undefined){
+            ref[freq] = new note.Note(freq, envelopes[env])
+        }
+        return ref[freq]
+    },
+    getFreqD(n, plus=0){
+        const t = n*2 + (n>3?-1:0)
+        return Math.exp(dNote*(t+12*plus))*261.626
+    }
+}
+
+module.exports={
+    Note,
+    EnvelopeADSR,
+    NoteLib,
+    Counter
 }
