8c40375764be67aff84291a0c962d1734819effc
TomoGaSukunai <292768995@qq.com>
2017-03-22 21:48:15 +0800

    Add tool to use WebGL

---
+++ Tools/Rainfall/rainfall.js
@@ -0,0 +1,40 @@
+var {FFT, IFFT, Amplitude} = require(__dirname+"/../../lib/fft")
+var drawing = require("../../lib/drawing")
+
+
+var canvas = document.getElementById("gaming")
+var gl = canvas.getContext("webgl")
+
+var vertices = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5]
+var vertex_buffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+var vertShader = gl.createShader(gl.VERTEX_SHADER)
+gl.shaderSource(vertShader, document.getElementById("shader-vs").text)
+gl.compileShader(vertShader)
+
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER)
+gl.shaderSource(fragShader, document.getElementById("shader-fs").text)
+gl.compileShader(fragShader)
+
+var shaderProgram = gl.createProgram()
+gl.attachShader(shaderProgram, vertShader)
+gl.attachShader(shaderProgram, fragShader)
+gl.linkProgram(shaderProgram)
+gl.useProgram(shaderProgram)
+
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer)
+var coord = gl.getAttribLocation(shaderProgram, "coordinates")
+gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0)
+gl.enableVertexAttribArray(coord)
+
+
+gl.clearColor(0.5, 0.5, 0.5, 0.9)
+gl.enable(gl.DEPTH_TEST)
+gl.clear(gl.COLOR_BUFFER_BIT)
+gl.viewport(0, 0, canvas.width, canvas.height)
+gl.drawArrays(gl.TRIANGLES, 0, 3)
+gl.drawArrays(gl.POINTS, 0, 3)
