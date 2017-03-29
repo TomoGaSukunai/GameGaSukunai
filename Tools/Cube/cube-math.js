044711b4c442ac77daec401571486048c3c09f53
TomoGaSukunai <292768995@qq.com>
2017-03-29 20:38:15 +0800

    Use let instead of var

--- Tools/Cube/cube-math.js
+++ Tools/Cube/cube-math.js
@@ -30,41 +30,41 @@ class V3{
 
 const bs = [-1,1]
 const vertices = []
-for(var x of bs){
-    for(var y of bs){
-        for(var z of bs){
+for(let x of bs){
+    for(let y of bs){
+        for(let z of bs){
             vertices.push(new V3(x,y,z))
         }
     }
 }
 
 const faces = []
-for(var b of bs){
+for(let b of bs){
     faces.push(new V3(b,0,0), new V3(0,b,0),new V3(0,0,b))
 }
 
 const faces_v =  faces.map(x=>[])
 const vertices_f = vertices.map(x=>[])
-for (var i in vertices){
-    var vertex = vertices[i]
-    for(var j in faces){
-        var face = faces[j]
+for (let i in vertices){
+    let vertex = vertices[i]
+    for(let j in faces){
+        let face = faces[j]
         if(vertex.inner(face) == 1){
             faces_v[j].push(i)
             vertices_f[i].push(j)
         }
     }
-    var fs = vertices_f[i].map(i=>faces[i])
+    let fs = vertices_f[i].map(i=>faces[i])
     if (! fs[2].rotate90(fs[1]).equals(fs[0])){
         vertices_f[i].reverse()
     }    
 }
 
-var trans_F = faces.map(x=>[])
-for (var i in faces){    
-    for(var j in faces_v){
-        var rotated = faces[j].rotate90(faces[i])
-        for (var k in faces){
+const trans_F = faces.map(x=>[])
+for (let i in faces){    
+    for(let j in faces_v){
+        let rotated = faces[j].rotate90(faces[i])
+        for (let k in faces){
             if (rotated.equals(faces[k])){
                 trans_F[i][j] = k
                 break
@@ -75,21 +75,21 @@ for (var i in faces){
 
 const mapping = faces_v.map(x=>x.map(x=>[]))
 
-for(var i in faces_v){
-    var face_v = faces_v[i]
-    for(var j in face_v){
-        var src = face_v[j]
-        var rotated = vertices[src].rotate90(faces[i])
-        var dst = 0
+for(let i in faces_v){
+    let face_v = faces_v[i]
+    for(let j in face_v){
+        let src = face_v[j]
+        let rotated = vertices[src].rotate90(faces[i])
+        let dst = 0
         for (dst in vertices){
             if(rotated.equals(vertices[dst])){                
                 break
             }
         }
 
-        for(var k = 0; k <3; k++){
+        for(let k = 0; k <3; k++){
             if (trans_F[i][vertices_f[src][0]] == vertices_f[dst][k]){
-                var maplet = [parseInt(src), parseInt(dst), k]
+                let maplet = [parseInt(src), parseInt(dst), k]
                 mapping[i][j] = maplet
                 //console.log("Rot along " + i+ " block: " +src + "->"+dst + "||" + k)
             }
