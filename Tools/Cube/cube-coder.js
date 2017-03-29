2d87301710df5bf608eeb9db2b834b61a8380e4a
TomoGaSukunai <292768995@qq.com>
2017-03-29 13:54:44 +0800

    Use let instead of var

--- Tools/Cube/cube-coder.js
+++ Tools/Cube/cube-coder.js
@@ -1,23 +1,23 @@
-var Coder = {
+const Coder = {
     getBytes(code){
-        var code = parseInt(code)
+        let code = parseInt(code)
         if (!isFinite(code)){
             throw new Error("illegal code")
         }
-        var r1 = code % 40320
-        var r2 = Math.floor(code / 40320)
-        var t = 40320
-        var ret = new Uint8Array(16)
-        var used = ret.slice(0,8).map(x=>false)
-        for(var i=0; i<8; i++){
+        let r1 = code % 40320
+        let r2 = Math.floor(code / 40320)
+        let t = 40320
+        let ret = new Uint8Array(16)
+        let used = ret.slice(0,8).map(x=>false)
+        for(let i=0; i<8; i++){
             t /= (8-i)
             ret[i] = Math.floor(r1/t)
             ret[15-i] = r2 % 3
             r1 %= t
             r2 /= 3
         }
-        for(var i=0; i<8; i++){
-            for (var j=0; j<=ret[i]; j++){
+        for(let i=0; i<8; i++){
+            for (let j=0; j<=ret[i]; j++){
                 if(used[j]){
                     ret[i]++
                 }
@@ -27,11 +27,11 @@ var Coder = {
         return ret
     },
     getCode(bytes){
-        var r1 = 0
-        var r2 = 0        
-        for(var i=0; i<8; i++){
-            var t = bytes[i]
-            for(var j=0; j<i; j++){
+        let r1 = 0
+        let r2 = 0        
+        for(let i=0; i<8; i++){
+            let t = bytes[i]
+            for(let j=0; j<i; j++){
                 if(bytes[i] > bytes[j]){
                     t--
                 }
