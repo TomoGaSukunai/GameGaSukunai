4fe0fd605b7b4878aee1ef415b410e360dc7d9d5
TomoGaSukunai <292768995@qq.com>
2017-04-06 22:59:15 +0800

    Add block instance

--- Tools/3d/3d.js
+++ Tools/3d/3d.js
@@ -40,6 +40,69 @@ function getUniformLocations(gl, shaderProgram, uniforms){
     return ret
 }
 
+
+function initBlocks(gl, attribs, uniforms){
+    var CubeMath = require("./cube-math")
+    var quad_indices = [0,2,1,0,3,2]
+    var conner_indices = [
+        0,1,2, 0,2,3,
+        0,3,4, 0,4,5,
+        0,5,6, 0,6,1,
+        ]
+    var realCubeColors = [[1,0.3,0],[1,1,1],[0,0.5,0],[1,0,0],[1,1,0],[0,0,1]]
+    var Blocks = []
+
+    for (var i in CubeMath.vertices_f){
+        var offset = 0
+        var vertices = []
+        var colors = []
+        var indices = []
+        var fs = CubeMath.vertices_f[i];
+        var vv = []
+        var v0 = CubeMath.vertices[i].toArray()
+        var sv = v0.map(x=>x*0.1)
+        for (var j=0; j<3; j++){        
+            vv[4*j] = v0
+            var faceArray = CubeMath.faces[fs[j]].toArray()
+            vv[4*j+1] = faceArray
+            vv[4*j+2] = faceArray
+            vv[4*j+3] = faceArray
+            indices.push(quad_indices.map(i=>i+offset))
+            offset += 4
+            var c = realCubeColors[fs[j]] //getColorByVec3(faceArray)
+            colors.push(c,c,c,c)//...quad_indices.map(x=>c))
+        }
+        for (var j=0; j<3; j++){        
+            vv[4*j+1] = vv[4*j+1].map((x,k)=>x+vv[4*((j+2)%3)+2][k])
+            vv[4*j+3] = vv[4*j+3].map((x,k)=>x+vv[4*((j+1)%3)+2][k])
+        }
+
+        vv.push([0,0,0])
+        for (var j=0; j<3;j++){
+            vv.push(vv[4*j+1])
+            vv.push(vv[4*j+2])        
+        }   
+            
+        indices.push(conner_indices.map(i=>i+offset))
+        offset += 7
+        var c = [0.4,0.4,0.4]
+        colors.push(c,c,c,c,c,c,c)
+        
+        vv = vv.map(v=>v.map((k,i)=>k + sv[i]))
+        
+        vertices.push(...vv)
+        vertices = vertices.reduce((a,b)=>[...a,...b])
+        indices = indices.reduce((a,b)=>[...a,...b])
+        colors = colors.reduce((a,b)=>[...a,...b])
+
+        var f0 = CubeMath.faces[fs[0]].toArray()
+        var Block = new Object3D(gl, indices, vertices, colors, "Block" + idx, attribs, uniforms)
+        Block.init()
+        
+        Blocks.push(Block)
+    }
+}
+
 function main(){
     var canvas = document.getElementById("gaming")
     var gl = initGL(canvas)
@@ -51,7 +114,10 @@ function main(){
     var attribs = getAttribLocations(gl, shaderProgram, 
         ["coordinates", "color"])
     var uniforms = getUniformLocations(gl, shaderProgram, 
-        ["Pmatrix", "Vmatrix", "Mmatrix", "Lmatrix"])    
+        ["Pmatrix", "Vmatrix", "Mmatrix", "Lmatrix"])
+    
+
+    var Blocks = initBlocks(gl, attribs, {LMatrix: uniforms.LMatrix})
 }
 
 
